7c8cbfc0abe7f359b0996b8ea2700ab3dab0a442
Max Pfeiffer <max@maxpfeiffer.ch>
2024-01-09 21:37:25 +0100

    Finalised the virtual environment handling for the tools tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,18 @@
+import os
+import sys
+from pathlib import Path
+from subprocess import run, CompletedProcess
 from uuid import uuid4
 
 import docker
 import pytest
 from docker.models.images import Image
+from docker.client import DockerClient
 from pytest_cookies.plugin import Result
-
-from tests.utils import setup_lock_file
 
 
 @pytest.fixture(scope="session")
-def docker_client() -> docker.client:
+def docker_client() -> DockerClient:
     return docker.client.from_env()
 
 
@@ -28,9 +31,70 @@ def hot_cookie(cookies_session) -> Result:
 
 
 @pytest.fixture(scope="session")
-def production_image(docker_client, hot_cookie: Result) -> str:
-    setup_lock_file(hot_cookie)
+def environment_variables(hot_cookie: Result) -> dict:
+    # Configuring paths
+    virtual_environment_path: Path = hot_cookie.project_path / ".venv"
+    virtual_environment_binary_path: Path = virtual_environment_path / "bin"
 
+    # Acquiring system environment variables
+    environment_variables: dict = os.environ.copy()
+
+    # "Deactivate" the old and "activate" the new virtual environment: strip the current virtual environment path and
+    # add the new virtual environment path.
+    # See: https://docs.python.org/3/library/venv.html#how-venvs-work
+    if sys.prefix != sys.base_prefix:
+        paths: list[str] = environment_variables["PATH"].split(os.pathsep)
+
+        old_virtual_environment_binary_path: str = f"{sys.prefix}/bin"
+        cleaned_paths: list[str] = [
+            item
+            for item in paths
+            if item != old_virtual_environment_binary_path
+        ]
+
+        cleaned_paths.insert(0, str(virtual_environment_binary_path))
+        environment_variables["PATH"] = os.pathsep.join(cleaned_paths)
+
+    # Setting the environment variable pointing to the new virtual environment
+    environment_variables["VIRTUAL_ENV"] = str(virtual_environment_path)
+
+    return environment_variables
+
+
+@pytest.fixture(scope="session")
+def lock_file_context(hot_cookie: Result, environment_variables: dict) -> dict:
+    # Create Poetry lock file for building Docker container
+    completed_process: CompletedProcess = run(
+        ["poetry", "lock"],
+        cwd=hot_cookie.project_path,
+        env=environment_variables,
+    )
+    if completed_process.returncode > 0:
+        raise Exception("Lock file could not be created with Poetry.")
+
+    return environment_variables
+
+
+@pytest.fixture(scope="session")
+def virtual_environment_context(
+    hot_cookie: Result, environment_variables: dict
+) -> dict:
+    # Install virtual environment with Poetry
+    completed_process: CompletedProcess = run(
+        ["poetry", "install", "--no-interaction", "--no-root"],
+        cwd=hot_cookie.project_path,
+        env=environment_variables,
+    )
+    if completed_process.returncode > 0:
+        raise Exception("Virtual environment could not be created with Poetry.")
+
+    return environment_variables
+
+
+@pytest.fixture(scope="session")
+def production_image(
+    docker_client: DockerClient, hot_cookie: Result, lock_file_context: dict
+) -> str:
     path: str = str(hot_cookie.project_path)
     tag: str = str(uuid4())
 
@@ -46,7 +110,7 @@ def production_image(docker_client, hot_cookie: Result) -> str:
 
 
 @pytest.fixture(scope="function")
-def cleaned_up_test_container(docker_client, request) -> None:
+def cleaned_up_test_container(docker_client: DockerClient, request) -> None:
     test_container_name: str = request.param
     yield test_container_name
     test_container = docker_client.containers.get(test_container_name)
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,21 +1,23 @@
 from subprocess import run, CompletedProcess
 
-from tests.utils import setup_virtual_environment
+from pytest_cookies.plugin import Result
 
 
-def test_run_black(hot_cookie) -> None:
-    environment_variables = setup_virtual_environment(hot_cookie)
-
+def test_run_black(
+    hot_cookie: Result, virtual_environment_context: dict
+) -> None:
     completed_process: CompletedProcess = run(
-        ["black", "."], cwd=hot_cookie.project_path, env=environment_variables
+        ["black", "."],
+        cwd=hot_cookie.project_path,
+        env=virtual_environment_context,
     )
     assert completed_process.returncode == 0
 
 
-def test_run_tests(hot_cookie) -> None:
-    environment_variables = setup_virtual_environment(hot_cookie)
-
+def test_run_tests(
+    hot_cookie: Result, virtual_environment_context: dict
+) -> None:
     completed_process: CompletedProcess = run(
-        ["pytest"], cwd=hot_cookie.project_path, env=environment_variables
+        ["pytest"], cwd=hot_cookie.project_path, env=virtual_environment_context
     )
     assert completed_process.returncode == 0
