7b5186450e476774cdf90809f07c78e0d71fe49b
Max Pfeiffer <max@maxpfeiffer.ch>
2024-08-17 15:06:10 +0200

    Switched to ruff in Cookiecutter project

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -5,19 +5,50 @@ from subprocess import CompletedProcess, run
 from pytest_cookies.plugin import Result
 
 
-def test_run_black(hot_cookie: Result, virtual_environment_context: dict) -> None:
-    """Test running black in generated project.
+def test_run_ruff(hot_cookie: Result, virtual_environment_context: dict) -> None:
+    """Test running ruff in generated project.
 
     :param hot_cookie:
     :param virtual_environment_context:
     :return:
     """
-    completed_process: CompletedProcess = run(
-        ["black", "."],
+    completed_check_process: CompletedProcess = run(
+        ["ruff", "check", "--exit-zero", "--config", "pyproject.toml"],
         cwd=hot_cookie.project_path,
         env=virtual_environment_context,
     )
-    assert completed_process.returncode == 0
+    assert completed_check_process.returncode == 0
+
+    completed_format_process: CompletedProcess = run(
+        ["ruff", "format", "--config", "pyproject.toml"],
+        cwd=hot_cookie.project_path,
+        env=virtual_environment_context,
+    )
+    assert completed_format_process.returncode == 0
+
+
+def test_run_pre_commit_handler(
+    hot_cookie: Result, virtual_environment_context: dict
+) -> None:
+    """Test running pre-commit handler in generated project.
+
+    :param hot_cookie:
+    :param virtual_environment_context:
+    :return:
+    """
+    completed_check_process: CompletedProcess = run(
+        ["git", "init"],
+        cwd=hot_cookie.project_path,
+        env=virtual_environment_context,
+    )
+    assert completed_check_process.returncode == 0
+
+    completed_check_process: CompletedProcess = run(
+        ["pre-commit", "run", "-a"],
+        cwd=hot_cookie.project_path,
+        env=virtual_environment_context,
+    )
+    assert completed_check_process.returncode == 0
 
 
 def test_run_tests(hot_cookie: Result, virtual_environment_context: dict) -> None:
